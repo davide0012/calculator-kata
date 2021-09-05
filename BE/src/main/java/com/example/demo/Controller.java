c4791297e65d4c17c8a614258e50ce22e67ca56e
davide.alberti <davide.alberti@marconirovereto.it>
2021-09-05 15:53:09 +0200

    Added crossorigin

--- BE/src/main/java/com/example/demo/Controller.java
+++ BE/src/main/java/com/example/demo/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.lang.NumberFormatException;
 @RestController
 public class Controller {
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/sum/{val1}/{val2}")
 	public Integer sum(@PathVariable("val1") final String val1,
 					   @PathVariable("val2") final String val2) {
