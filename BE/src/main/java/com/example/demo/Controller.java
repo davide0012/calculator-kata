df8872bc64c1d2f997fec5dd5e544a98c3d71f26
davide.alberti <davide.alberti@marconirovereto.it>
2021-09-05 14:35:43 +0200

    Check url parameter and return result

--- BE/src/main/java/com/example/demo/Controller.java
+++ BE/src/main/java/com/example/demo/Controller.java
@@ -3,13 +3,24 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.lang.NumberFormatException;
 
 @RestController
 public class Controller {
 	
 	@GetMapping("/sum/{val1}/{val2}")
-	public Integer sum(@PathVariable("val1") String val1,
-					   @PathVariable("val2") String val2) {
-		return 0;
+	public Integer sum(@PathVariable("val1") final String val1,
+					   @PathVariable("val2") final String val2) {
+		int first = 0;
+		int second = 0;
+		
+		try {
+			first = Integer.parseInt(val1);
+			second = Integer.parseInt(val2);
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+		
+		return first + second;
 	}
 }
